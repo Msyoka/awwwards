0ef19c9b2b3ed0e570e2454295379255983ee2dd
Msyoka <daviemumo37@gmail.com>
2021-05-30 21:50:41 +0300

    add imports to settings

--- awards/settings.py
+++ awards/settings.py
@@ -10,6 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+import django_heroku
+import dj_database_url
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
