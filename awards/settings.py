9a30e7c281502a00b47a3c0cdf74eb99306d1a79
Msyoka <daviemumo37@gmail.com>
2021-06-01 21:15:36 +0300

    d

--- awards/settings.py
+++ awards/settings.py
@@ -106,7 +106,12 @@ DATABASES = {
        
     }
 }
-
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
 WHITENOISE_USE_FINDERS = True
 
 # Password validation
